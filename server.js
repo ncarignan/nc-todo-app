3da2b90afe7e279d3b19dbc0031269a866438e1d
Nicholas Carignan <nick.carignan@sbcglobal.net>
2018-10-31 13:59:22 -0700

    adding updates

--- server.js
+++ server.js
@@ -3,24 +3,14 @@
 // Application Dependencies
 const express = require('express');
 const pg = require('pg');
-const methodOverride = require('method-override');
 
 // Application Setup
 const app = express();
 const PORT = process.env.PORT || 3000;
-const DATABASE_URL = process.env.DATABASE_URL || 'postgres://ncarignan:password@localhost:5432/todo_app_demo'
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://ncarignan:password@localhost:5432/todo_app_demo';
 
 // Utilize ExpressJS functionality to parse the body of the request
 app.use(express.urlencoded({extended: true}));
-
-app.use(methodOverride((request, response) => {
-  if (request.body && typeof request.body === 'object' && '_method' in request.body) {
-    // look in urlencoded POST bodies and delete it
-    let method = request.body._method;
-    delete request.body._method;
-    return method;
-  }
-}))
 
 // Database Setup
 const client = new pg.Client(DATABASE_URL);
@@ -38,8 +28,6 @@ app.get('/tasks/:task_id', getOneTask);
 app.get('/add', showForm);
 
 app.post('/add', addTask);
-
-app.put('/update/:task_id', updateTask);
 
 app.get('*', (req, res) => res.status(404).send('This route does not exist'));
 
@@ -77,19 +65,6 @@ function addTask(request, response) {
 
   return client.query(SQL, values)
     .then(response.redirect('/'))
-    .catch(err => handleError(err, response));
-}
-
-function updateTask(request, response) {
-  // destructure variables
-  let {title, description, category, contact, status, due} = request.body;
-  // need SQL to update the specific task that we were on
-  let SQL = `UPDATE tasks SET title=$1, description=$2, category=$3, contact=$4, status=$5, due=$6 WHERE id=$7;`;
-  // use request.params.task_id === whatever task we were on
-  let values = [title, description, category, contact, status, due, request.params.task_id];
-
-  client.query(SQL, values)
-    .then(response.redirect(`/tasks/${request.params.task_id}`))
     .catch(err => handleError(err, response));
 }
 
